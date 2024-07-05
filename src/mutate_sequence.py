a2631d620c1421acee53bb21ec5a6a47033d69f9
SonPham <108428215+son-h-pham@users.noreply.github.com>
2024-07-05 18:27:58 -0500

    Update mutate_sequence.py to add locked positions

--- src/mutate_sequence.py
+++ src/mutate_sequence.py
@@ -1,44 +1,67 @@
 import numpy as np
 import copy
-import pdb
 
-def search_aa(seqlen, peptide_sequence, searched_seqs):
+def search_aa(seqlen, peptide_sequence, searched_seqs, locked_positions=None):
     '''
+    Search for a new amino acid sequence
     '''
     restypes = np.array(['A', 'R', 'N', 'D', 'C', 'Q', 'E', 'G', 'H', 'I',
-                 'L', 'K', 'M', 'F', 'P', 'S', 'T', 'W', 'Y', 'V' ])
-
+                 'L', 'K', 'M', 'F', 'P', 'S', 'T', 'W', 'Y', 'V'])
     #Go through a shuffled version of the positions and aas
-    for pi in np.random.choice(np.arange(seqlen),seqlen, replace=False):
-        for aa in np.random.choice(restypes,len(restypes), replace=False):
-            new_seq = copy.deepcopy(peptide_sequence)
-            new_seq = new_seq[:pi]+aa+new_seq[pi+1:]
+    for pi in np.random.choice(np.arange(seqlen), seqlen, replace=False):
+        if locked_positions and pi in locked_positions:
+            continue  # Skip locked positions
+        for aa in np.random.choice(restypes, len(restypes), replace=False):
+            new_seq = list(peptide_sequence)
+            new_seq[pi] = aa
+            new_seq = ''.join(new_seq)
             if new_seq in searched_seqs:
                 #Recursion - next will use new seq as peptide_sequence
-                search_aa(seqlen, new_seq, searched_seqs)
+                return search_aa(seqlen, new_seq, searched_seqs, locked_positions)
             else:
-                return new_seq
+                # Ensure locked positions are still correct
+                if locked_positions:
+                    for pos, locked_aa in locked_positions.items():
+                        if new_seq[pos] != locked_aa:
+                            break
+                    else:
+                        return new_seq
+                else:
+                    return new_seq
+    return None  # Return None if no valid sequence is found
 
-def mutate_sequence(peptide_sequence, sequence_scores):
-    '''Mutate the amino acid sequence randomly
+def mutate_sequence(peptide_sequence, sequence_scores, locked_positions=None):
+    '''Mutate the amino acid sequence randomly, respecting locked positions
     '''
-
     restypes = np.array(['A', 'R', 'N', 'D', 'C', 'Q', 'E', 'G', 'H', 'I',
-                 'L', 'K', 'M', 'F', 'P', 'S', 'T', 'W', 'Y', 'V' ])
-
+                 'L', 'K', 'M', 'F', 'P', 'S', 'T', 'W', 'Y', 'V'])
     seqlen = len(peptide_sequence)
     searched_seqs = sequence_scores['sequence']
+    
     #Mutate seq
     seeds = [peptide_sequence]
     #Go through a shuffled version of the positions and aas
     for seed in seeds:
-        for pi in np.random.choice(np.arange(seqlen),seqlen, replace=False):
-            for aa in np.random.choice(restypes,len(restypes), replace=False):
-                new_seq = copy.deepcopy(seed)
-                new_seq = new_seq[:pi]+aa+new_seq[pi+1:]
+        for pi in np.random.choice(np.arange(seqlen), seqlen, replace=False):
+            if locked_positions and pi in locked_positions:
+                continue  # Skip locked positions
+            for aa in np.random.choice(restypes, len(restypes), replace=False):
+                new_seq = list(seed)
+                new_seq[pi] = aa
+                new_seq = ''.join(new_seq)
                 if new_seq in searched_seqs:
                     continue
                 else:
-                    return new_seq
-
+                    # Ensure locked positions are still correct
+                    if locked_positions:
+                        for pos, locked_aa in locked_positions.items():
+                            if new_seq[pos] != locked_aa:
+                                break
+                        else:
+                            return new_seq
+                    else:
+                        return new_seq
         seeds.append(new_seq)
+    
+    # If no valid mutation is found, try searching for a completely new sequence
+    return search_aa(seqlen, peptide_sequence, searched_seqs, locked_positions)
